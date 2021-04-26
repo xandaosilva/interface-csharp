bbfc7d8e774bcea4b265e97157258ba877f48432
Alexandre Rogério <xandaosilva93@hotmail.com>
2021-04-26 18:52:55 -0300

    develop Service and Program class

--- Aluguel-de-carros/Program.cs
+++ Aluguel-de-carros/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Aluguel_de_carros.Entities;
+using Aluguel_de_carros.Services;
 using System;
 using System.Globalization;
 namespace Aluguel_de_carros
@@ -10,25 +11,23 @@ namespace Aluguel_de_carros
             CultureInfo CI = CultureInfo.InvariantCulture;
 
             Console.WriteLine("Enter rental data");
-            
             Console.Write("Car model: ");
             string model = Console.ReadLine();
             Console.Write("Pickup(dd/MM/yyyy hh:mm): ");
             DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CI);
             Console.Write("Return(dd/MM/yyyy hh:mm): ");
             DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CI);
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CI);
 
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
-
-
-            Console.Write("Enter price per hour: ");
-            double pricePerHour = double.Parse(Console.ReadLine(), CI);
-
-            Console.WriteLine("Enter price per day: ");
-            double pricePerDay = double.Parse(Console.ReadLine(), CI);
+            RentalService rentalService = new RentalService(hour, day);
+            rentalService.ProcessInvoice(carRental);
 
             Console.WriteLine("INVOICE:");
-            
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
--- Aluguel-de-carros/Services/RentalService.cs
+++ Aluguel-de-carros/Services/RentalService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aluguel_de_carros.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,6 +10,8 @@ namespace Aluguel_de_carros.Services
         public double PricePerHour { get; private set; }
         public double PricePerDay { get; private set; }
 
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
         public RentalService()
         {
         }
@@ -17,6 +20,24 @@ namespace Aluguel_de_carros.Services
         {
             PricePerHour = pricePerHour;
             PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+            double basicPayment = 0.0;
+
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _brazilTaxService.Tax(basicPayment);
+            carRental.Invoice = new Invoice(basicPayment, tax);
         }
     }
 }
