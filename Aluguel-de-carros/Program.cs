7d3c024a74d9befe0099fa867be323318e511f6e
Alexandre Rogério <xandaosilva93@hotmail.com>
2021-04-26 17:34:26 -0300

    first commit

---
+++ Aluguel-de-carros/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+namespace Aluguel_de_carros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+        }
+    }
+}
